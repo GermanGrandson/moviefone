b1c658f7cc2104122ec6180a69f1e158f80e58c9
German Grandson <ganieto1@gmail.com>
2016-05-23 18:00:19 -0400

    raty functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,12 @@ class MoviesController < ApplicationController
 
   def show
     @reviews = Review.where(movie_id: @movie.id).order('created_at DESC')
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating)
+    end
   end
 
   def new
